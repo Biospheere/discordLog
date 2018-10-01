19eff54454277219772de016df967d7a8135d184
Biospheere <niklas.public@gmx.de>
2018-10-01 19:05:01 +0200

    Added UserBanEvent

--- src/main/java/de/biosphere/log/listener/UserListener.java
+++ src/main/java/de/biosphere/log/listener/UserListener.java
@@ -2,6 +2,7 @@ package de.biosphere.log.listener;
 
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.member.NicknameChangedEvent;
+import sx.blah.discord.handle.impl.events.guild.member.UserBanEvent;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.util.EmbedBuilder;
 
@@ -31,6 +32,17 @@ public class UserListener {
         }
         embedBuilder.withFooterText("@" + event.getUser().getName() + "#" + event.getUser().getDiscriminator());
         embedBuilder.withFooterIcon(event.getUser().getAvatarURL());
+        IChannel channel = event.getGuild().getChannelsByName("logger").stream().findFirst().orElse(null);
+        if(channel != null){
+            channel.sendMessage(embedBuilder.build());
+        }
+    }
+
+    @EventSubscriber
+    public void onUserBan(UserBanEvent event){
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.withColor(Color.RED);
+        embedBuilder.appendDesc(event.getUser().getName() + "#" + event.getUser().getDiscriminator() + " wurde gebannt");
         IChannel channel = event.getGuild().getChannelsByName("logger").stream().findFirst().orElse(null);
         if(channel != null){
             channel.sendMessage(embedBuilder.build());
